4fd38ae77703bb8215e9a2530e6b83e5c14ce7ca
Aashi Garg <aashigarg2504@gmail.com>
2026-01-22 22:31:58 +0530

    Initial commit: hero section

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import HeroPage from "./components/HeroPage";
+// import About from "./components/About";
+// import Schedule from "./components/Schedule";
+// import TracksPage from "./components/TracksPage";
+// import Events from "./components/Events";
+// import Judges from "./components/Judges";
+// import Sponsors from "./components/Sponsors";
+// import FAQ from "./components/FAQ";
+// import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <HeroPage />
+      {/* <About />
+      <Schedule />
+      <TracksPage />
+      <Events />
+      <Judges />
+      <Sponsors />
+      <FAQ />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default App;
